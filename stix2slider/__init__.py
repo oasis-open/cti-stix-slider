44f02b5c8167ca669352db078c834a258d4d0ec2
Emmanuelle Vargas-Gonzalez <emmanuelle@mitre.org>
2018-06-07 08:13:44 -0400

    Integrate stix-validator API into stix2slider. closes #2

--- stix2slider/__init__.py
+++ stix2slider/__init__.py
@@ -1,40 +1,117 @@
 import io
 import json
+import logging
+import sys
 
 import stix2
+from sdv import codes, errors, scripts
 
 from stix2slider.convert_stix import convert_bundle
-from stix2slider.options import setup_logger
+from stix2slider.options import setup_logger, get_validator_options
 import stix2slider.utils  # flake8: noqa
+
+# Module-level logger
+log = logging.getLogger(__name__)
 
 
 def slide_file(fn, encoding="utf-8"):
     setup_logger(fn)
+    validator_options = get_validator_options()
 
     with io.open(fn, "r", encoding=encoding) as json_data:
         json_content = json.load(json_data)
 
     obj = stix2.parse(json_content)
-    # TODO: validate STIX 2.0 content - what to do if it is invalid??
     stix_package = convert_bundle(obj)
 
     if stix_package:
-        return stix_package.to_xml(encoding=None)
+        xml = stix_package.to_xml(encoding=None)
+        validator_options.in_files = io.StringIO(xml)
+
+        try:
+            scripts.set_output_level(validator_options)
+
+            validation_results = scripts.validate_file(
+                validator_options.in_files,
+                validator_options
+            )
+            results = {stix_package.id_: validation_results}
+
+            # Print stix-validator results
+            scripts.print_results(results, validator_options)
+        except (errors.ValidationError, IOError) as ex:
+            scripts.error(
+                "Validation error occurred: '%s'" % str(ex),
+                codes.EXIT_VALIDATION_ERROR
+            )
+        except Exception:
+            log.exception("Fatal error occurred", extra={'ecode': 0})
+            sys.exit(codes.EXIT_FAILURE)
+
+        return xml
 
 
 def slide_string(string):
     obj = stix2.parse(string)
     setup_logger(obj["id"])
+    validator_options = get_validator_options()
 
     stix_package = convert_bundle(obj)
 
     if stix_package:
-        return stix_package.to_xml(encoding=None)
+        xml = stix_package.to_xml(encoding=None)
+        validator_options.in_files = io.StringIO(xml)
+
+        try:
+            scripts.set_output_level(validator_options)
+
+            validation_results = scripts.validate_file(
+                validator_options.in_files,
+                validator_options
+            )
+            results = {stix_package.id_: validation_results}
+
+            # Print stix-validator results
+            scripts.print_results(results, validator_options)
+        except (errors.ValidationError, IOError) as ex:
+            scripts.error(
+                "Validation error occurred: '%s'" % str(ex),
+                codes.EXIT_VALIDATION_ERROR
+            )
+        except Exception:
+            log.exception("Fatal error occurred", extra={'ecode': 0})
+            sys.exit(codes.EXIT_FAILURE)
+
+        return xml
 
 
 def slide_bundle(bundle):
     setup_logger(bundle["id"])
     stix_package = convert_bundle(bundle)
+    validator_options = get_validator_options()
 
     if stix_package:
-        return stix_package.to_xml(encoding=None)
+        xml = stix_package.to_xml(encoding=None)
+        validator_options.in_files = io.StringIO(xml)
+
+        try:
+            scripts.set_output_level(validator_options)
+
+            validation_results = scripts.validate_file(
+                validator_options.in_files,
+                validator_options
+            )
+            results = {stix_package.id_: validation_results}
+
+            # Print stix-validator results
+            scripts.print_results(results, validator_options)
+        except (errors.ValidationError, IOError) as ex:
+            scripts.error(
+                "Validation error occurred: '%s'" % str(ex),
+                codes.EXIT_VALIDATION_ERROR
+            )
+        except Exception:
+            log.exception("Fatal error occurred", extra={'ecode': 0})
+            sys.exit(codes.EXIT_FAILURE)
+
+        return xml
--- stix2slider/options.py
+++ stix2slider/options.py
@@ -1,6 +1,9 @@
+import argparse
 import logging
 import os
+import shlex
 
+from sdv import scripts, validators
 from six import text_type
 
 ALL_OPTIONS = None
@@ -100,6 +103,8 @@ class SliderOptions(object):
         cmd_args: An instance of ``argparse.Namespace`` containing options
             supplied on the command line.
         file_: Input file to be elevated.
+        validator_args: If set, these values will be used to create a
+            ValidationOptions instance if requested.
         enable: Messages to enable.
         disable: Messages to disable.
         silent: If set, no stix2-slider log messages will be emitted.
@@ -110,7 +115,7 @@ class SliderOptions(object):
         All messages are turned on by default.
     """
     def __init__(self, cmd_args=None, file_=None,
-                 no_squirrel_gaps=False,
+                 no_squirrel_gaps=False, validator_args="",
                  enable="", disable="",
                  silent=False, message_log_directory=None,
                  output_directory=None, log_level="INFO"):
@@ -119,6 +124,7 @@ class SliderOptions(object):
             if hasattr(cmd_args, "file_"):
                 self.file_ = cmd_args.file_
             self.no_squirrel_gaps = cmd_args.no_squirrel_gaps
+            self.validator_args = cmd_args.validator_args
 
             self.enable = cmd_args.enable
             self.disable = cmd_args.disable
@@ -131,6 +137,7 @@ class SliderOptions(object):
         else:
             self.file_ = file_
             self.no_squirrel_gaps = no_squirrel_gaps
+            self.validator_args = validator_args
             self.enable = enable
             self.disable = disable
             self.silent = silent
@@ -167,6 +174,19 @@ def initialize_options(slider_args=None):
             warn("Both console and output log have disabled messages.", 202)
 
 
+def get_validator_options():
+    if ALL_OPTIONS:
+        validator_parser = _get_validator_arg_parser()
+        validator_args = validator_parser.parse_args(
+            shlex.split(get_option_value("validator_args"))
+        )
+
+        _validate_validator_args(validator_args)
+        options = _set_validation_options(validator_args)
+
+        return options
+
+
 def get_option_value(option_name):
     if ALL_OPTIONS and hasattr(ALL_OPTIONS, option_name):
         return getattr(ALL_OPTIONS, option_name)
@@ -191,6 +211,152 @@ def msg_id_enabled(msg_id):
         return msg_id in get_option_value("enabled")
     else:
         return not (msg_id in get_option_value("disabled"))
+
+
+def _set_validation_options(args):
+    """Populates an instance of ``ValidationOptions`` from the `args` param.
+    Args:
+        args (argparse.Namespace): The arguments parsed and returned from
+            ArgumentParser.parse_args().
+    Returns:
+        Instance of ``ValidationOptions``.
+    """
+    options = scripts.ValidationOptions()
+    options.schema_validate = True
+
+    if options.schema_validate and args.profile:
+        options.profile_validate = True
+
+    # best practice options
+    options.best_practice_validate = args.best_practices
+
+    # input options
+    options.lang_version = args.lang_version
+    options.schema_dir = args.schema_dir
+    options.in_profile = args.profile
+    options.recursive = args.recursive
+    options.use_schemaloc = args.use_schemaloc
+    options.huge_tree = args.huge_tree
+
+    # output options
+    options.json_results = args.json
+    options.quiet_output = args.quiet
+
+    # validation class options
+    options.xml_validation_class = validators.STIXSchemaValidator
+
+    return options
+
+
+def _validate_validator_args(args):
+    """Checks that valid and compatible command line arguments were passed into
+    the application.
+    Args:
+        args (argparse.Namespace): The arguments parsed and returned from
+            ArgumentParser.parse_args().
+    Raises:
+        ArgumentError: If invalid or incompatible command line arguments were
+            passed into the application.
+    """
+    schema_validate = True
+    profile_validate = False
+
+    if schema_validate and args.profile:
+        profile_validate = True
+
+    if all((args.lang_version, args.use_schemaloc)):
+        raise scripts.ArgumentError(
+            "Cannot set both --stix-version and --use-schemalocs"
+        )
+
+    if args.profile and not profile_validate:
+        raise scripts.ArgumentError(
+            "Profile specified but no validation options specified."
+        )
+
+
+def _get_validator_arg_parser():
+    """Initializes and returns an argparse.ArgumentParser instance for the
+    stix-validator application.
+
+    Returns:
+        Instance of ``argparse.ArgumentParser``
+    """
+    parser = argparse.ArgumentParser(
+        description="STIX Document Validator v{}".format(sdv.__version__)
+    )
+
+    parser.add_argument(
+        "--stix-version",
+        dest="lang_version",
+        default=None,
+        help="The version of STIX to validate against"
+    )
+
+    parser.add_argument(
+        "--schema-dir",
+        dest="schema_dir",
+        default=None,
+        help="Schema directory. If not provided, the STIX schemas bundled "
+             "with the stix-validator library will be used."
+    )
+
+    parser.add_argument(
+        "--use-schemaloc",
+        dest="use_schemaloc",
+        action='store_true',
+        default=False,
+        help="Use schemaLocation attribute to determine schema locations."
+    )
+
+    parser.add_argument(
+        "--best-practices",
+        dest="best_practices",
+        action='store_true',
+        default=False,
+        help="Check that the document follows authoring best practices."
+    )
+
+    parser.add_argument(
+        "--profile",
+        dest="profile",
+        default=None,
+        help="Path to STIX Profile .xlsx file."
+    )
+
+    parser.add_argument(
+        "--quiet",
+        dest="quiet",
+        action="store_true",
+        default=False,
+        help="Only print results and errors if they occur."
+    )
+
+    parser.add_argument(
+        "--json-results",
+        dest="json",
+        action="store_true",
+        default=False,
+        help="Print results as raw JSON. This also sets --quiet."
+    )
+
+    parser.add_argument(
+        "--recursive",
+        dest="recursive",
+        action="store_true",
+        default=False,
+        help="Recursively descend into input directories."
+    )
+
+    parser.add_argument(
+        "--huge-tree",
+        dest="huge_tree",
+        action="store_true",
+        default=False,
+        help="Disable libxml2 security restrictions on XML document size."
+    )
+
+    return parser
 
 
 # These codes are aligned with elevator_log_messages spreadsheet.
